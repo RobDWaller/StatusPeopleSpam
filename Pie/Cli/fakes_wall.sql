31fbc767d51b4e0593b0f34338eaf1ed07942b4c
Rob Waller <rob@statuspeople.com>
2016-04-05 10:01:22 +0100

    fixed faker wall bugs

--- Pie/Cli/fakes_wall.sql
+++ Pie/Cli/fakes_wall.sql
@@ -1,11 +1,10 @@
-USE statuspeople_spam;
+TRUNCATE statuspeople_spam.spsp_fakers_wall;
 
-TRUNCATE spsp_fakers_wall;
-
-INSERT INTO spsp_fakers_wall (twitterid, screen_name, avatar, spam, potential, checks, followers, updated)
-SELECT ss.twitterid, ui.screen_name, ui.avatar, ss.spam, ss.potential, ss.checks, ss.followers, ss.updated
-FROM spsp_spam_scores AS ss
-JOIN spsp_user_info AS ui ON ss.twitterid = ui.twitterid
-WHERE (ss.spam/ss.checks) * 100 > 90
-ORDER BY updated DESC
+INSERT INTO statuspeople_spam.spsp_fakers_wall (twitterid, screen_name, avatar, spam, potential, checks, followers, updated)
+SELECT ss.twitterid, ss.screen_name, ss.avatar, ss.spam, ss.potential, ss.checks, ss.followers, ss.score_date
+FROM statuspeople_spam_api.stpsa_spam_scores AS ss
+WHERE (ss.spam/ss.checks) * 100 > 80
+AND ss.followers >= 1000
+AND ss.live = 1
+ORDER BY ss.score_date DESC
 LIMIT 0, 300;
